cd7ee9eeae91eb188da50988cab0e620d38090b1
meisterfuu <info@meister-fuu.de>
2017-04-22 16:42:26 +0200

    TilePosCalc

--- core/src/de/dogedev/ld38/ashley/ComponentMappers.java
+++ core/src/de/dogedev/ld38/ashley/ComponentMappers.java
@@ -2,6 +2,7 @@ package de.dogedev.ld38.ashley;
 
 import com.badlogic.ashley.core.ComponentMapper;
 import de.dogedev.ld38.ashley.components.PositionComponent;
+import de.dogedev.ld38.ashley.components.RenderComponent;
 
 /**
  * Created by elektropapst on 22.04.2017.
@@ -9,4 +10,5 @@ import de.dogedev.ld38.ashley.components.PositionComponent;
 public class ComponentMappers {
 
     public static final ComponentMapper<PositionComponent> position = ComponentMapper.getFor(PositionComponent.class);
+    public static final ComponentMapper<RenderComponent> render = ComponentMapper.getFor(RenderComponent.class);
 }
