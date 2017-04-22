383f75f43d079644f338e6b2a11cde2e389f9a21
Christopher Brumann <christopher.brumann@googlemail.com>
2017-04-22 14:09:03 +0200

    Initial renderer

--- core/src/de/dogedev/ld38/screens/GameScreen.java
+++ core/src/de/dogedev/ld38/screens/GameScreen.java
@@ -3,8 +3,17 @@ package de.dogedev.ld38.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.renderers.HexagonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import de.dogedev.ld38.Key;
 import de.dogedev.ld38.Statics;
 
@@ -14,13 +23,45 @@ import de.dogedev.ld38.Statics;
 public class GameScreen implements Screen {
 
     TextureRegion texture;
-
     SpriteBatch batch;
+
+    private TiledMap map;
+    private OrthographicCamera camera;
+    private HexagonalTiledMapRenderer renderer;
+
 
     @Override
     public void show() {
         batch = new SpriteBatch();
         texture = Statics.asset.getTextureAtlasRegion(Key.OBJECTS_ANVIL);
+
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 1280, 720);
+
+        map = new TiledMap();
+        map.getProperties().put("hexsidelength", 68);
+        map.getProperties().put("staggeraxis", "y");
+        MapLayers layers = map.getLayers();
+        TiledMapTile[] tiles = new TiledMapTile[1];
+
+        tiles[0] = new StaticTiledMapTile(Statics.asset.getTextureAtlasRegion(Key.TILES_MEDIEVAL_MEDIEVAL_CHURCH));
+
+        for (int l = 0; l < 1; l++) {
+            TiledMapTileLayer layer = new TiledMapTileLayer(10, 10, 120, 140);
+            for (int y = 0; y < 10; y++) {
+                for (int x = 0; x < 10; x++) {
+                    TiledMapTileLayer.Cell cell = new TiledMapTileLayer.Cell();
+                    cell.setTile(tiles[0]);
+                    layer.setCell(x, y, cell);
+                }
+            }
+            layers.add(layer);
+        }
+
+        renderer = new HexagonalTiledMapRenderer(map);
 
     }
 
@@ -28,9 +69,12 @@ public class GameScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(texture, 100, 100);
-        batch.end();
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
+//        batch.begin();
+//        batch.draw(texture, 100, 100);
+//        batch.end();
 
     }
 
