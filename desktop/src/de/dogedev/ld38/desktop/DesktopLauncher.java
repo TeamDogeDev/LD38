6b26764d4c0aa16c991abe8e4fa32dcbcf75293e
Christopher Brumann <christopher.brumann@googlemail.com>
2017-04-23 20:59:36 +0200

    Title

--- desktop/src/de/dogedev/ld38/desktop/DesktopLauncher.java
+++ desktop/src/de/dogedev/ld38/desktop/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		config.width = 1280;
 		config.height = 720;
 		config.samples = 8;
+		config.title = "Tiny Battlehex";
 		new LwjglApplication(new LDGame(), config);
 	}
 }
