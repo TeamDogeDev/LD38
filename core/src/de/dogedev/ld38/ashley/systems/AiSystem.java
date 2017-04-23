4496396e9be76f69da254cdd46eb0f2334f72613
Christopher Brumann <christopher.brumann@googlemail.com>
2017-04-23 17:21:29 +0200

    foobarbaz

--- core/src/de/dogedev/ld38/ashley/systems/AiSystem.java
+++ core/src/de/dogedev/ld38/ashley/systems/AiSystem.java
@@ -7,10 +7,12 @@ import com.badlogic.ashley.systems.IntervalSystem;
 import com.badlogic.ashley.utils.ImmutableArray;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import de.dogedev.ld38.Statics;
 import de.dogedev.ld38.ashley.ComponentMappers;
 import de.dogedev.ld38.ashley.components.PlayerComponent;
 import de.dogedev.ld38.ashley.components.SpawnComponent;
 import de.dogedev.ld38.ashley.components.TilePositionComponent;
+import de.dogedev.ld38.assets.GameSettings;
 import de.dogedev.ld38.screens.GameScreen;
 
 /**
@@ -49,50 +51,71 @@ public class AiSystem extends IntervalSystem {
 //            }
 //        }
 //        if (spawn != null) {
-            for (int i = 0; i < fields.size(); i++) {
-                if(MathUtils.randomBoolean(.5f)) continue;
-                Entity entity = fields.get(i);
-                if (ComponentMappers.player.get(entity).player.equals(player)) {
-                    TilePositionComponent sourcePos = ComponentMappers.tilePos.get(entity);
-                    //Send?
 
 
-                    //Target field
-                    Entity target = null;
-                    int dist = Integer.MAX_VALUE;
-                    for (Entity t : fields) {
-                        if (!ComponentMappers.player.get(t).player.equals(player)) {
-                            TilePositionComponent g1 = ComponentMappers.tilePos.get(t);
+        int numPlayer = 0;
 
-                            Vector2.X.set(g1.x, g1.y);
-                            Vector2.Y.set(sourcePos.x, sourcePos.y);
-                            int dist2 = (int) Vector2.X.dst(Vector2.Y);
-                            if(dist2 < 0) dist *= -1;
-                            if(dist2 <= dist){
-                                dist = dist2;
-                                target = t;
-                            }
+        for (Entity fieldEntity : fields) {
+            PlayerComponent playerComponent = ComponentMappers.player.get(fieldEntity);
+            if (playerComponent.player == PlayerComponent.PLAYER.A) {
+                numPlayer++;
+            }
+        }
+        float owningPercentagePlayer = ((float) numPlayer) / (Statics.settings.tilesX * Statics.settings.tilesY);
+
+        float skipChance = 1 - Statics.settings.difficulty; // difficulty = 1 = hard -> skipchance = 0
+        skipChance -= .5 * MathUtils.clamp(owningPercentagePlayer, 0.0, Math.min(owningPercentagePlayer, (1 - Statics.settings.difficulty)));
+        skipChance = MathUtils.clamp(skipChance, 0.2f, 0.8f);
+
+        Vector2 tmpVector = new Vector2();
+        Vector2 tmpVector2 = new Vector2();
+        for (int i = 0; i < fields.size(); i++) {
+            // smaller = harder
+
+            if (MathUtils.randomBoolean(skipChance)) continue;
+            Entity entity = fields.get(i);
+            if (ComponentMappers.player.get(entity).player.equals(player)) {
+                TilePositionComponent sourcePos = ComponentMappers.tilePos.get(entity);
+                //Send?
+
+
+                //Target field
+                Entity target = null;
+                int dist = Integer.MAX_VALUE;
+                for (Entity t : fields) {
+                    if (!ComponentMappers.player.get(t).player.equals(player)) {
+                        TilePositionComponent g1 = ComponentMappers.tilePos.get(t);
+
+                        tmpVector.set(g1.x, g1.y);
+                        tmpVector2.set(sourcePos.x, sourcePos.y);
+                        int dist2 = (int) tmpVector.dst(Vector2.Y);
+                        if (dist2 < 0) dist *= -1;
+                        if (dist2 <= dist) {
+                            dist = dist2;
+                            target = t;
                         }
-                    }
-                    if(target != null) {
-                        TilePositionComponent targetPos = ComponentMappers.tilePos.get(target);
-                        int targetTileX = sourcePos.x;
-                        int targetTileY = sourcePos.y;
-                        if (targetTileX > targetPos.x && !(targetTileY < targetPos.y)) {
-                            targetTileX -= 1;
-                        } else if (targetTileX < targetPos.x) {
-                            targetTileX += 1;
-                        }
-                        if (targetTileY > targetPos.y) {
-                            targetTileY -= 1;
-                        } else if (targetTileY < targetPos.y) {
-                            targetTileY += 1;
-                        }
-
-                        gameScreen.spawnWarrior(new Vector2(sourcePos.x, sourcePos.y), new Vector2(targetTileX, targetTileY), PlayerComponent.PLAYER.B, 40);
                     }
                 }
+
+                if (target != null) {
+                    TilePositionComponent targetPos = ComponentMappers.tilePos.get(target);
+                    int targetTileX = sourcePos.x;
+                    int targetTileY = sourcePos.y;
+                    if (targetTileX > targetPos.x && !(targetTileY < targetPos.y)) {
+                        targetTileX -= 1;
+                    } else if (targetTileX < targetPos.x) {
+                        targetTileX += 1;
+                    }
+                    if (targetTileY > targetPos.y) {
+                        targetTileY -= 1;
+                    } else if (targetTileY < targetPos.y) {
+                        targetTileY += 1;
+                    }
+
+                    gameScreen.spawnWarrior(new Vector2(sourcePos.x, sourcePos.y), new Vector2(targetTileX, targetTileY), PlayerComponent.PLAYER.B, 40);
+                }
             }
+        }
 //        }
     }
 }
